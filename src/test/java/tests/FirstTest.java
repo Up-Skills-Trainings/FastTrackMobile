269338f15fdb669ad1d8231ebb12131d1e9137a0
OscarSah <oscar@cybertekschool.com>
2024-09-03 20:27:25 -0400

    initial commit

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstTest {
+    public static String projectRoot = System.getProperty("user.dir");
+    private AppiumDriverLocalService service; // Appium server object
+    @BeforeEach
+    public void startServer(){
+        AppiumServiceBuilder builder = new AppiumServiceBuilder();
+        builder.usingAnyFreePort();
+
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+    }
+    @AfterEach
+    public void killServer(){
+        if(service != null){
+            service.stop();
+        }
+    }
+
+    @Test
+    public void serverTest(){
+        String serviceUrl = service.getUrl().toString();
+        System.out.println("serviceUrl = " + serviceUrl);
+    }
+
+    @Test
+    public void test01() throws MalformedURLException {
+
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setApp(projectRoot+"/sample.apk")
+                .setAppPackage("org.wikipedia")
+                .setAppActivity("org.wikipedia.main.MainActivity");
+        AndroidDriver driver = new AndroidDriver(
+                // The default URL in Appium 1 is http://127.0.0.1:4723/wd/hub
+                new URL(service.getUrl().toString()), options
+        );
+        try {
+            // driver.findElement(new AppiumBy.ByAccessibilityId("Wikipedia")).click();
+            driver.getPageSource();
+        } finally {
+            driver.quit();
+        }
+    }
+}
